f7b9f77a6d9d5f8e73825aa7ef5d3a1a6ee14acb
ryan3780 <guemhong3780@gmail.com>
2019-01-06 16:29:47 +0900

    구구단

---
+++ gugu1.py
@@ -0,0 +1,12 @@
+# def gugu():
+#     a = int(input("몇 단이요?"))
+#     for i in range(1,10):
+#         print("{} * {} = {}".format(a,i,a*i))
+#
+# gugu()
+
+# 코너가 말한 5x5 2차원 배열
+a = [[i for i in range(j, j + 5)] for j in range(1, 26, 5)]
+
+for item in a :
+    print(item)
